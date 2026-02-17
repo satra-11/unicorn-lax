ecc1ef243bdfbb0af5b0083212d898ee292cec9f
satra-11 <syuzuki546@gmail.com>
2026-02-17 16:13:38 +0900

    chore: Add Prettier for consistent code formatting and integrate it with ESLint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,18 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    ignores: ['public/**/*', 'dist/**/*'],
+  },
+).append({
+  // Prettier integration
+  // manually adding prettier config since withNuxt returns a composer
+  plugins: {
+    prettier: (await import('eslint-plugin-prettier')).default,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    // turn off conflicting rules
+    ...(await import('eslint-config-prettier')).default.rules,
+  },
+})

e5f846c40e73e00477cda2ee7048b6e9d19d2a91
satra-11 <syuzuki546@gmail.com>
2026-09-21 00:14:31 +0900

    feat: remove face model selection

--- app/utils/types.ts
+++ app/utils/types.ts
@@ -23,7 +23,6 @@ export interface Photo {
   thumbnail?: Blob
   excluded?: boolean
   hash?: string
-  detectionModel?: 'ssd'
   matchedSubjects?: string[] // Added to track which selected subjects appear in this photo
 }
 
